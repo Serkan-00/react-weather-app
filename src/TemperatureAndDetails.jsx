786fa0dcfa5cde505228d35f9fc98d8d1a8d2a32
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-07-01 18:13:33 +0100

    Details of temperature

--- src/TemperatureAndDetails.jsx
+++ src/TemperatureAndDetails.jsx
@@ -17,26 +17,36 @@ function TemperatureAndDetails() {
         <p>Sunny</p>
     </div>
 
-    <div className="flex flex-row items-center justify-between
-    text-white py-3">
-        <img src="http://openweathermap.org/img/wn/01d@2x.png"
-        alt=""
-        className="w-20"
-        />
+    <div className="flex flex-col items-center justify-between text-white py--5">
+      <img src="http://openweathermap.org/img/wn/01d@2x.png"
+      alt="Weather Icon"
+      className="w-20"
+      />
 
         <p className="text-5xl"> 40°</p>
         <div className="flex flex-col space-y-2"></div>
+          
           <div className="flex font-light text-sm items-center justify-center">
             <UilTemperature size={18} className="mr-1" />
             Real fell:
             <span className="font-medium ml-1">32°</span>
+          </div>
 
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilTear size={18} className="mr-1" />
+            Humidity:
+            <span className="font-medium ml-1">65%</span>
+          </div>
 
+          <div className="flex font-light text-sm items-center justify-center">
+            <UilWind size={18} className="mr-1" />
+            Wind Speed:
+            <span className="font-medium ml-1">11km</span>
           </div>
 
     
         </div>
-  </div>
+    </div>
 
 }
 
