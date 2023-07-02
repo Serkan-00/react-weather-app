f1d041407bf8de3daa7d8ba98bd981150894c5f5
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-07-02 20:23:40 +0100

    Before real data

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import Inputs from "./Inputs";
 import TimeAndLocation from "./TimeAndLocation";
 import TemperatureAndDetails from "./TemperatureAndDetails";
 import Forecast from "./Forecast"; 
+import getFormattedWeatherData from "./weatherService";
 
 
 function App() {
+
+  const fetchWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "london" }) ;
+    console.log(data);
+  };
+   
+  fetchWeather() ;
+
+
+  
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl 
     shadow-gray-400">
@@ -24,3 +35,5 @@ function App() {
 };
 
 export default App;
+
+
