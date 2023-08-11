905891114aaf3d492bc559f794328e878e846a0f
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-08-11 21:24:34 +0100

    able to change to F

--- src/Inputs.jsx
+++ src/Inputs.jsx
@@ -4,9 +4,28 @@ import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons"
 function Inputs({ setQuery, units, setUnits }) {
   const [city, setCity] = useState(""); 
 
+  const handleUnitsChange = (e) => { 
+    const selectedUnit = e.currentTarget.name; 
+    if (units !== selectedUnit) setUnits(selectedUnit); 
+  }; 
+
   const handleSearchClick = () => { 
     if (city !== '') setQuery({q: city})
-  }
+  }; 
+
+  const handleLocationClick = () => { 
+    if (navigator.geolocation) { 
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude; 
+        let lon = position.coords.longitude; 
+
+        setQuery({
+          lat,
+          lon,
+        });
+      }); 
+    }
+  }; 
 
   return ( 
     <div className="flex flex-row justify-center my-6">
@@ -24,19 +43,19 @@ function Inputs({ setQuery, units, setUnits }) {
 
             
             <UilLocationPoint size={25} 
-            className="text-white cursor-pointer transition ease-out hover:scale-125" /> 
+            className="text-white cursor-pointer transition ease-out hover:scale-125" onClick={handleLocationClick} /> 
         </div>
 
         <div className="flex flex-row w-1/4 items-center justify-center"> 
           <button 
           name="metric" 
-          className="text-xl text-white font-light transition ease-out hover:scale-125">
+          className="text-xl text-white font-light transition ease-out hover:scale-125" onClick={handleUnitsChange}>
             °C
           </button>
           <p className="text-xl text-white mx-1"> | </p>
           <button 
           name="imperial" 
-          className="text-xl text-white font-light transition ease-out hover:scale-125">
+          className="text-xl text-white font-light transition ease-out hover:scale-125" onClick={handleUnitsChange}>
             °F
           </button>
         
