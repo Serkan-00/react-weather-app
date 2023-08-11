bb89e3356c685820d2cb39c0860107e44764ab8a
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-08-11 20:54:15 +0100

    Cities are searchable

--- src/Inputs.jsx
+++ src/Inputs.jsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { UilSearch, UilLocationPoint } from '@iconscout/react-unicons'
+import React, { useState } from "react"
+import { UilSearch, UilLocationPoint } from "@iconscout/react-unicons"
 
-function Inputs() {
+function Inputs({ setQuery, units, setUnits }) {
+  const [city, setCity] = useState(""); 
+
+  const handleSearchClick = () => { 
+    if (city !== '') setQuery({q: city})
+  }
+
   return ( 
     <div className="flex flex-row justify-center my-6">
         <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
             <input 
+            value={city}
+            onChange={(e) => setCity(e.currentTarget.value)}
             type="text"
             placeholder="Search..." 
             className="text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:lowercase" 
             />
             
             <UilSearch size={25} 
-            className="text-white cursor-pointer transition ease-out hover:scale-125" /> 
+            className="text-white cursor-pointer transition ease-out hover:scale-125" onClick={handleSearchClick} /> 
+
             
             <UilLocationPoint size={25} 
             className="text-white cursor-pointer transition ease-out hover:scale-125" /> 
