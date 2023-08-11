0634629665efeee8a42b599a3f228a4d9ffc20d9
serkancoskun01@hotmail.com <serkancoskun01@hotmail.com>
2023-08-11 15:14:50 +0100

    fixed issue

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ import Inputs from "./Inputs";
 import TimeAndLocation from "./TimeAndLocation";
 import TemperatureAndDetails from "./TemperatureAndDetails";
 import Forecast from "./Forecast"; 
-import getFormattedWeatherData from "./weatherService";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
 
 
 function App() {
 
-  const fetchWeather = async () => {
-    const data = await getFormattedWeatherData({ q: "london" }) ;
-    console.log(data);
-  };
+  const [query, setQuery] = useState({q: "berlin"})
+  const [units, setUnits] = useState("metric")
+  const [weather, setWeather] = useState(null)
+
+  useEffect(() => {
+   const fetchWeather = async () => {
+    await getFormattedWeatherData({...query, units}).then(
+    (data) => {
+      setWeather(data); 
+    });
+   };
    
-  fetchWeather() ;
+   fetchWeather() ;
+  }, [query, units]); 
 
 
   
